6c7a0fb74aaf05161dfcc7d363ee18b020c33e0f
Kris Sankaran <sankaran.kris@gmail.com>
2019-07-03 21:06:52 -0400

    split into training and data scripts

--- src/train.py
+++ src/train.py
@@ -1,92 +1,4 @@
 #!/usr/bin/env python
-from glob import glob
-from model_unet_gan import *
-from torch.utils import data
-import datetime
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import torch
-
-datapath= '/data/' #where training and test data is located
-temp = "T"
-rh = "RH"
-u = "U"
-v = "V"
-ts = "TS"
-r_chan = "Reflectance_680"
-g_chan = "Reflectance_551"
-b_chan = "Reflectance_443"
-H,W = 256, 256 # num channels, height, width
-
-# read out meto data given an npz file
-def readin_epic_meto(fname):
-    fh = np.load(fname)
-    t = fh['T'][:]
-    r_hum = fh['RH'][:]
-    u_wnd = fh['U'][:]
-    v_wnd = fh['V'][:]
-    s_temp = np.expand_dims(fh['TS'][:],axis =0)
-    fh.close()
-    output = np.concatenate((t,r_hum,u_wnd,v_wnd,s_temp), axis = 0)
-    output[np.where(np.isnan(output))] = 0
-    return output
-
-def readin_epic_img(fname):
-    fh = np.load(fname)
-    r = np.expand_dims(fh[r_chan][:],axis =0)
-    g = np.expand_dims(fh[g_chan][:],axis =0)
-    b = np.expand_dims(fh[b_chan][:],axis =0)
-    output = np.concatenate((r,g,b), axis = 0)
-    output[np.where(np.isnan(output))] = 0
-    return output
-
-
-class Dataset(data.Dataset):
-    def __init__(self, root, x_field, y_field, start=0,stop=-1, Cout = 3, Cin=41):
-
-        self.Cin     = Cin
-        self.Cout    = Cout
-        self.x_files = []
-        self.y_files = []
-        prefix       = 'epic_1b_'
-
-        x_path = "{}/{}".format(root,x_field)
-        y_path = "{}/{}".format(root,y_field)
-
-        files = sorted(glob(x_path+"/**.npz"))
-        files = files[start:stop]
-        self.x_files += files
-
-        # get matching y's
-        for i,file in enumerate(files):
-            path,b    = os.path.split(file) # split off basename
-            timestamp = b[b.find(prefix) : b.find("_Collocated_MERRA2.npz")]
-            y_file = "{}/{}.npz".format(y_path,timestamp)
-            self.y_files.append(y_file)
-
-        self.N = len(self.x_files)
-
-        self.x = torch.zeros([self.N,self.Cin,H,W],dtype=torch.float)
-        self.y = torch.zeros([self.N,self.Cout,H,W],dtype=torch.float)
-
-        print()
-
-    def __len__(self):
-        return self.N
-
-    def __getitem__(self,index):
-        im_x = readin_epic_meto(self.x_files[index])
-        im_y = readin_epic_img(self.y_files[index])
-
-        x = torch.from_numpy(im_x).float()
-        y = torch.from_numpy(im_y).float()
-        return x,y
-
-trainset = Dataset(datapath, 'metos','imgs')
-#trainset = Dataset(datapath,[2014],fieldA,fieldB,start=0)
-#testset  = Dataset(datapath,[2013],fieldA,fieldB,start=0,stop=1000)
-
 from hyperopt import STATUS_OK, STATUS_FAIL
 from tensorboardX import SummaryWriter
 from datetime import datetime
@@ -96,7 +8,6 @@ from torch.utils import data
 import time
 import os
 import random
-
 import scipy.spatial.distance as distance
 from scipy import stats
 
@@ -115,7 +26,6 @@ class gan_trainer:
         print(self.runpath)
 
     def make_directories(self):
-
         self.trialdir  = "{}/trial_{}".format(self.runpath, self.trial_number)
         self.logdir    = "{}/log".format(self.trialdir)
         self.imgdir    = "{}/images".format(self.trialdir)
@@ -125,9 +35,6 @@ class gan_trainer:
         os.makedirs(self.imgdir  , exist_ok=True)
 
     def run_trail(self,params):
-
-        # unpack parameters
-
         trial_start_time = time.time()
         self.trial_number+=1
         lr_d           =     params['lr_d']
@@ -158,13 +65,11 @@ class gan_trainer:
         self.make_directories()
 
         # initialize objects
-
         self.writer = SummaryWriter(self.logdir)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         print(self.device)
 
         trainset.Cin = self.cin
-        #testset.Cin  = self.cin
 
         self.trainloader= torch.utils.data.DataLoader(trainset, batch_size=self.batchsize, shuffle=True,  num_workers=8)
 #        self.testloader = torch.utils.data.DataLoader(testset,  batch_size=128, shuffle=False, num_workers=8)
@@ -343,6 +248,8 @@ class gan_trainer:
 
 
 
+datapath= '/data/'
+trainset = Dataset(datapath, 'metos','imgs')
 trainer = gan_trainer()
 
 params1   = {
