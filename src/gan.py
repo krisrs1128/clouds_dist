ca6e7601779fef90f27d1f64cdeac005dba22191
Kris Sankaran <sankaran.kris@gmail.com>
2019-07-29 10:48:34 -0400

    use same name as in json, for n_blocks

--- src/gan.py
+++ src/gan.py
@@ -6,10 +6,10 @@ from src.unets import UNet
 
 
 class GAN(nn.Module):
-    def __init__(self, Cin, Cout, n_channels, nblocks, kernel_size, dropout):
+    def __init__(self, Cin, Cout, n_channels, n_blocks, kernel_size, dropout):
         super(GAN, self).__init__()
 
-        self.g = unet(Cin, Cout, n_channels, nblocks, kernel_size, dropout)
+        self.g = unet(Cin, Cout, n_channels, n_blocks, kernel_size, dropout)
         self.d = Discriminator(Cout, n_channels=16, nlevels=4)
 
         self.g.apply(self.init_weights)
