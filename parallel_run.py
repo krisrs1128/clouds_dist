c39719c6be036809e07d5aca1bccef3a64af112f
vict0rsch <vsch@pm.me>
2019-10-16 19:37:21 -0400

    add test_mode for this file

--- parallel_run.py
+++ parallel_run.py
@@ -267,6 +267,11 @@ if __name__ == "__main__":
         default="default",
         help="what template to use to write the sbatch files",
     )
+    parser.add_argument(
+        "--test_mode",
+        action="store_true",
+        help="don't actually run the sbatch files, just create everything",
+    )
 
     opts = parser.parse_args()
 
@@ -355,5 +360,6 @@ if __name__ == "__main__":
         file = run_dir / f"run-{sbp['conf_name']}.sh"
         with file.open("w") as f:
             f.write(template)
-        print(subprocess.check_output(f"sbatch {str(file)}", shell=True))
+        if not opts.test_mode:
+            print(subprocess.check_output(f"sbatch {str(file)}", shell=True))
         print("In", str(run_dir), "\n")
