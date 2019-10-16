e129189a3aa37b7d7834212baff018b9104341a0
mustafaghali <wad_alghali@hotmail.com>
2019-10-16 15:33:42 -0400

    remove the nans from the stats computation and implement removeNans transform

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,7 +2,6 @@ from src.data import EarthData
 import numpy as np
 import torch
 
-
 class Rescale:
     def __init__(self, data_path, batch_size, num_workers=3, verbose=1):
         self.data_path = data_path
@@ -20,7 +19,6 @@ class Rescale:
             num_workers=self.num_workers,
         )
         self.means, self.ranges = self.get_stats()
-
     def expand_as(self, a, b):
         """Repeat a vector b that gives 1 value per channel so that it
         can be used in elementwise computations with a. a.shape[1] should
@@ -59,36 +57,81 @@ class Rescale:
 
         sample["real_imgs"] = (sample["real_imgs"] - img_mean_expand) / img_range_expand
         sample["metos"] = (sample["metos"] - metos_mean_expand) / metos_range_expand
-
-        sample["real_imgs"][torch.isnan(sample["real_imgs"])] = 0.0
-        sample["real_imgs"][torch.isinf(sample["real_imgs"])] = 0.0
-        sample["metos"][torch.isnan(sample["metos"])] = 0.0
-        sample["metos"][torch.isinf(sample["metos"])] = 0.0
         return sample
 
     def get_stats(self):
         maxes = {}
         mins = {}
         means = {}
+        norm = {}
         for i, batch in enumerate(self.data_loader):
             torch.cuda.empty_cache()
             for k, v in batch.items():
                 v = v.to(self.device)
                 if i == 0:
-                    means[k] = v.mean(dim=(0, 2, 3))
-                    maxes[k] = v.max(0)[0].max(1)[0].max(1)[0]
-                    mins[k] = v.min(0)[0].min(1)[0].min(1)[0]
-
+                    means[k] = torch.tensor(
+                        [
+                            v[:, c, :][~torch.isnan(v[:, c, :])].mean()
+                            for c in range(v.shape[1])
+                        ],
+                        dtype=torch.float,
+                    ).to(self.device)
+                    maxes[k] = torch.tensor(
+                        [
+                            (v[:, c, :][~torch.isnan(v[:, c, :])]).max()
+                            for c in range(v.shape[1])
+                        ],
+                        dtype=torch.float,
+                    ).to(self.device)
+                    mins[k] = torch.tensor(
+                        [
+                            (v[:, c, :][~torch.isnan(v[:, c, :])]).min()
+                            for c in range(v.shape[1])
+                        ],
+                        dtype=torch.float,
+                    ).to(self.device)
+                    norm[k] = torch.tensor(
+                        [
+                            v[:, c, :][~torch.isnan(v[:, c, :])].numel()
+                            for c in range(v.shape[1])
+                        ],
+                        dtype=torch.float,
+                    ).to(self.device)
                 else:
-                    n = i * self.batch_size
-                    m = len(v)
-                    means[k] *= n / (n + m)
 
-                    means[k] += v.sum(dim=(0, 2, 3)) / (
-                        (n + m) * v.shape[2] * v.shape[3]
-                    )
-                    cur_max = v.max(0)[0].max(1)[0].max(1)[0]
-                    cur_min = v.min(0)[0].min(1)[0].min(1)[0]
+                    # count all elements that aren't nans per channel
+                    m = torch.tensor(
+                        [
+                            v[:, i, :][~torch.isnan(v[:, i, :])].numel()
+                            for i in range(v.shape[1])
+                        ],
+                        dtype=torch.float,
+                    ).to(self.device)
+                    means[k] *= norm[k] / (norm[k] + m)
+                    means[k] += torch.tensor(
+                        [
+                            (v[:, c, :][~torch.isnan(v[:, c, :])]).sum()
+                            for c in range(v.shape[1])
+                        ],
+                        dtype=torch.float,
+                    ).to(self.device) / (norm[k] + m)
+
+                    norm[k] += m
+
+                    cur_max = torch.tensor(
+                        [
+                            v[:, i, :][~torch.isnan(v[:, i, :])].max()
+                            for i in range(v.shape[1])
+                        ],
+                        dtype=torch.float,
+                    ).to(self.device)
+                    cur_min = torch.tensor(
+                        [
+                            v[:, i, :][~torch.isnan(v[:, i, :])].min()
+                            for i in range(v.shape[1])
+                        ],
+                        dtype=torch.float,
+                    ).to(self.device)
 
                     maxes[k][maxes[k] < cur_max] = cur_max[maxes[k] < cur_max]
                     mins[k][mins[k] > cur_min] = cur_min[mins[k] > cur_min]
@@ -105,7 +148,7 @@ class Rescale:
         # calculate ranges and avoid cuda multiprocessing by bringing tensors back to cpu
         stats = (
             {k: v.to("cpu") for k, v in means.items()},
-            {k: (maxes[k] - v).to("cpu") for k, v in mins.items()},
+            {k: (maxes[k] - v).to("cpu") for k, v in mins.items()},  # return range
         )
         torch.cuda.empty_cache()
         return stats
@@ -122,3 +165,39 @@ class Crop:
             for k, v in sample.items()
         }
         return result
+
+
+class Zoom:
+    def __init__(self, crop_size=20):
+        self.crop_size = crop_size
+        return
+
+    def __call__(self, sample):
+        dim = list(sample.items())[0][1].size()[-1]
+        upsample = torch.nn.UpsamplingNearest2d(
+            scale_factor=dim / (dim - 2 * self.crop_size)
+        )
+
+        crop_result = {
+            k: v[:, self.crop_size : -self.crop_size, self.crop_size : -self.crop_size]
+            if k == "real_imgs"
+            else v
+            for k, v in sample.items()
+        }
+        zoom_result = {
+            k: upsample(v.unsqueeze(0)).squeeze() if k == "real_imgs" else v
+            for k, v in crop_result.items()
+        }
+        return zoom_result
+
+
+class RemoveNans:
+    def __init__(self):
+        return
+
+    def __call__(self, sample):
+        sample["real_imgs"][torch.isnan(sample["real_imgs"])] = 0.0
+        sample["real_imgs"][torch.isinf(sample["real_imgs"])] = 0.0
+        sample["metos"][torch.isnan(sample["metos"])] = 0.0
+        sample["metos"][torch.isinf(sample["metos"])] = 0.0
+        return sample
