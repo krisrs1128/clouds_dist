7bc88fb115f21c44454eb5c3d6ffe0e209aaf722
vict0rsch <vsch@pm.me>
2019-10-24 11:36:28 -0400

    remove line

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -67,7 +67,6 @@ class Rescale:
         return
 
     def __call__(self, sample):
-        dim = list(sample.items())[0][1].size()[1:]
         upsample = torch.nn.UpsamplingNearest2d(size=self.res)
         rescaled_sample = {
             k: upsample(v.unsqueeze(0)).squeeze(0) for k, v in sample.items()
