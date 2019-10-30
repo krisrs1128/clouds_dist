b68962b72ad0586584bc9f54f322a15170185edd
Kris Sankaran <sankaran.kris@gmail.com>
2019-10-29 20:17:13 -0400

    slightly more generic: write from iterator incrementally

--- shared/inspect.py
+++ shared/inspect.py
@@ -2,18 +2,18 @@
 """
 Utilities to Study Model Results
 """
-import torch
-import numpy as np
-import seaborn as sns
-import argparse
 from src.data import get_loader, get_transforms
-import src.gan as gan
-from src.utils import get_opts
 from src.stats import get_stats
+from src.utils import get_opts
+import argparse
+import numpy as np
 import pathlib
+import seaborn as sns
+import src.gan as gan
+import torch
 
 
-def infer(model, loader, out_dir=None, M=10):
+def infer(model, loader, M=1000):
     """
     Predictions on a Subset
 
@@ -55,12 +55,53 @@ def histogram(x, sample_frac=0.3, out_path=None):
     sns.distplot(x[indices]).figure.savefig(pathlib.Path(out_path, "figure.png"))
 
 
-def loader_from_run(opts_path):
+def save_line(z, f, round_level=4):
+    """
+    1D Array -> String for line in CSV
+    """
+    str_fun = lambda z: ",".join(np.round(z, round_level).astype(str))
+    f.write(str_fun(z.flatten().numpy()))
+    f.write("\n")
+
+
+def loader_gen(loader, key="metos"):
+    """
+    Wrapper for Loaders
+    """
+    for m, batch in enumerate(loader):
+        for i in range(len(batch[key])):
+            yield batch[key][i]
+
+
+def tensor_gen(z):
+    """
+    Tensor -> Iterator
+    """
+    for i in range(len(z)):
+        yield z[i]
+
+
+def save_iterator(iterator, out_path="x.csv", crop_ix=(124, 127), M=1000):
+    """
+    Save Iterator to File Incrementally
+    """
+    with open(out_path, "w") as f:
+        for m, sample in enumerate(iterator):
+            if m > M: break
+            print(f"Extracting batch {m}/{M}")
+            cropped = sample[:, crop_ix[0]:crop_ix[1], crop_ix[0]:crop_ix[1]]
+            save_line(cropped, f)
+
+
+def loader_from_run(opts_path, data_path=None):
     """
     Get Loader (with transforms) from Experiment
     """
     opts = get_opts(opts_path)
-    opts["data"]["path"] = opts["data"]["original_path"]
+    if not data_path:
+        opts["data"]["path"] = opts["data"]["original_path"]
+    else:
+        opts["data"]["path"] = data_path
 
     print("getting transforms")
     transfs = get_transforms(opts)
@@ -76,7 +117,6 @@ def model_from_run(opts_path, checkpoints_dir, model_name):
     Get Model from Experiment
     """
     opts = get_opts(opts_path)
-    opts["data"]["path"] = opts["data"]["original_path"]
     model_path = pathlib.Path(checkpoints_dir, model_name)
 
     state = torch.load(model_path)["state_dict"]
@@ -97,7 +137,7 @@ if __name__ == '__main__':
         "-c",
         "--conf_path",
         type=str,
-        default= "/scratch/sankarak/clouds/extragrad-run--8/run_0/extragrad-run.yaml",
+        default= "/scratch/sankarak/clouds/regression-run--3/run_0/short-run-cropped.yaml",
         help="The full path to the configuration file in the experiment that you want to analyze."
     )
     parser.add_argument(
@@ -112,8 +152,10 @@ if __name__ == '__main__':
     # get the model and loader
     checkpoints_dir = pathlib.Path(pathlib.Path(opts.conf_path).parent, "checkpoints")
     model = model_from_run(opts.conf_path, checkpoints_dir, opts.model_pt)
-    loader = loader_from_run(opts.conf_path)
+    loader = loader_from_run(opts.conf_path, "/scratch/sankarak/data/clouds_small/")
 
     # make predictions and summarize
     y_hat = infer(model, loader)
-    histogram(y_hat)
+    save_iterator(tensor_gen(y_hat), "y_hat.csv")
+    save_iterator(loader_gen(loader, "real_imgs"), "y.csv")
+    save_iterator(loader_gen(loader, "metos"), "x.csv")
