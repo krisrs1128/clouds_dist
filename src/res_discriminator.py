4d4710f3377bbfeb1f16025ae43fed7ca76a125f
vict0rsch <vsch@pm.me>
2019-10-31 11:33:44 -0400

    use multi-disc

--- src/res_discriminator.py
+++ src/res_discriminator.py
@@ -165,3 +165,56 @@ class SpectralNorm(nn.Module):
     def forward(self, *args):
         self._update_u_v()
         return self.module.forward(*args)
+
+
+class MultiDiscriminator(nn.Module):
+    def __init__(self, in_channels=3, device=None):
+        super(MultiDiscriminator, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, normalize=True):
+            """Returns downsampling layers of each discriminator block"""
+            layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
+            if normalize:
+                layers.append(nn.InstanceNorm2d(out_filters))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        # Extracts three discriminator models
+        self.models = nn.ModuleList()
+        for i in range(3):
+            self.models.add_module(
+                "disc_%d" % i,
+                nn.Sequential(
+                    *discriminator_block(in_channels, 64, normalize=False),
+                    *discriminator_block(64, 128),
+                    *discriminator_block(128, 256),
+                    *discriminator_block(256, 512),
+                    nn.Conv2d(512, 1, 3, padding=1)
+                ),
+            )
+
+        self.downsample = nn.AvgPool2d(
+            in_channels, stride=2, padding=[1, 1], count_include_pad=False
+        )
+        self.apply(weights_init_normal)
+
+    def compute_loss(self, x, gt):
+        """Computes the MSE between model output and scalar gt"""
+        loss = sum([torch.mean((out - gt) ** 2) for out in self.forward(x)])
+        return loss
+
+    def forward(self, x):
+        outputs = []
+        for m in self.models:
+            outputs.append(m(x))
+            x = self.downsample(x)
+        return outputs
+
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
