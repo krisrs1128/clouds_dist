16bd5eee1c7681ced45e5f39fcb92c6cb4bf2e59
Kris Sankaran <sankaran.kris@gmail.com>
2019-11-14 10:40:11 -0500

    allow project ID argument in wandb upload

--- shared/wand_upload.py
+++ shared/wand_upload.py
@@ -14,12 +14,19 @@ parser.add_argument(
     default=".",
     help="Directory containing all the wandb dry runs to upload",
 )
-parsed_opts = parser.parse_args()
-p = pathlib.Path(parsed_opts.runs_dir) # make this an argument
+parser.add_argument(
+    "-p",
+    "--project",
+    type=str,
+    default="clouds_dist",
+    help="Project ID to associate the runs to."
+)
+opts = parser.parse_args()
+p = pathlib.Path(opts.runs_dir) # make this an argument
 paths = list(p.rglob('*wandb*'))
 
 for p in paths:
     try:
-        subprocess.check_output(f"wandb sync {str(p.parent)}  --project=clouds_dist", shell=True)
+        subprocess.check_output(f"wandb sync {str(p.parent)} --project={opts.project}", shell=True)
     except:
         print(f"could not upload {p.parent}")
