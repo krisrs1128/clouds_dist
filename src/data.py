8cc7fc03d0b5e7d94a0343dff651a7ff818fae83
Kris Sankaran <sankaran.kris@gmail.com>
2019-11-28 11:23:59 -0500

    new dataset class for low clouds data

--- src/data.py
+++ src/data.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from pathlib import Path
+import datetime
 import numpy as np
+import re
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
@@ -13,45 +15,6 @@ from src.preprocessing import (
     Resize,
     Quantize,
 )
-
-class LowClouds(Dataset):
-    """
-    Low Clouds / Metereology Data
-
-    Each index corresponds to one 128 x 128 low cloud image, along with 8
-    meteorological variables. A separate metadata field stores parsed
-    information from the filenames.
-
-    Example
-    -------
-    >>> LowClouds("/scratch/sankarak/data/low_clouds/")
-    """
-    def __init__(self, data_dir, load_limit=-1, transform=None):
-        self.data = {
-            "metos": np.load(pathlib.Path(data_dir, "meto.npy")),
-            "real_imgs": np.load(pathlib.Path(data_dir, "train.npy"))
-        }
-        self.ids = np.load(pathlib.Path(data_dir, "files.npy"))
-        self.transform = transform
-
-        if load_limit != -1:
-            self.ids = self.ids[:load_limit]
-            self.data["metos"] = self.data["metos"][:load_limit]
-            self.data["real_imgs"] = self.data["real_imgs"][:load_limit]
-
-    def __len__(self):
-        return len(self.ids)
-
-    def __getitem__(self, i):
-        data = {
-            "metos": self.data["metos"][:, i]
-            "real_imgs": self.data["real_imgs"][:, :, i]
-        }
-
-        if self.transform:
-            data = self.transform(data)
-        return data, self.ids[i]
-
 
 class EarthData(Dataset):
     """
@@ -146,6 +109,56 @@ def process_sample(data):
         ]
     )
     return {"real_imgs": torch.Tensor(imgs), "metos": torch.Tensor(metos)}
+
+
+class LowClouds(Dataset):
+    """
+    Low Clouds / Metereology Data
+
+    Each index corresponds to one 128 x 128 low cloud image, along with 8
+    meteorological variables. A separate metadata field stores parsed
+    information from the filenames.
+
+    Example
+    -------
+    >>> clouds = LowClouds("/scratch/sankarak/data/low_clouds/")
+    """
+    def __init__(self, data_dir, load_limit=-1, transform=None):
+        self.data = {
+            "metos": np.load(Path(data_dir, "meto.npy")),
+            "real_imgs": np.load(Path(data_dir, "train.npy"))
+        }
+
+        # some metadata
+        files = [np.load(Path(data_dir, "files.npy"))]
+        self.ids = [Path(str(f)).name for f in files]
+        self.metadata = [{"id": s, "date": parse_dates(s)} for s in self.ids]
+        self.transform = transform
+
+        if load_limit != -1:
+            self.ids = self.ids[:load_limit]
+            self.data["metos"] = self.data["metos"][:load_limit]
+            self.data["real_imgs"] = self.data["real_imgs"][:load_limit]
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, i):
+        data = {
+            "metos": self.data["metos"][:, i],
+            "real_imgs": self.data["real_imgs"][:, :, i]
+        }
+
+        if self.transform:
+            data = self.transform(data)
+        return data, self.ids[i]
+
+
+def parse_dates(s):
+    pattern = "(20[0-9][0-9])([0-9][0-9])([0-9]+).([0-9][0-9])([0-9][0-9])" # regexr.com/4ponn
+    groups = (int(g) for g in re.search(pattern, s).groups())
+    return datetime.datetime(*groups)
+
 
 def get_nan_value(transfs):
     nan_value = "raw"
